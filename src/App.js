90452ea53f6d805271eab3d500c231c95f015126
Krishna Dhingra <dkrishna1608@gmail.com>
2021-08-08 02:00:54 +0530

    Initial COmmit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar">
+      <nav>
+
+        <div className="hamburger_icon"><img src="menu.png" alt="menu"/></div>
+        <div className="logo"><img src="https://otmanezahhari.github.io/Audiophile/assets/shared/desktop/logo.svg" alt="Cart"/></div>
+        <ul>
+          <li className="first_link"><a>HOME</a></li>
+          <li><a>HEADPHONES</a></li>
+          <li><a>SPEAKERS</a></li>
+          <li><a>EARPHONES</a></li>
+        </ul>
+        <div className="shoping_cart_icon_outer">
+          <img src="https://otmanezahhari.github.io/Audiophile/assets/shared/desktop/icon-cart.svg" alt="Cart"/>
+        </div>
+      </nav>
     </div>
   );
 }
