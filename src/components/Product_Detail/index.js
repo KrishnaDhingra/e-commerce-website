66a102d05321cde8bea089d9b141c608e4afd920
Krishna Dhingra <dkrishna1608@gmail.com>
2021-08-17 15:18:30 +0530

    Adding the showcase header component

--- src/components/Product_Detail/index.js
+++ src/components/Product_Detail/index.js
@@ -10,25 +10,5 @@ import Showcase_Header from '../Showcase_Header/index.js'
 import Showcase from '../Showcase/index.js'
 
 function Product_Detail(){
-
-    const [product_name, setName] = useState('YX1 WIRELESS EARPHONES')
-    const [product_info, setInfo] = useState('')
-    const hello = 'hello'
-
-    console.log(product_data)
-
-    product_data.map(element =>{
-        if(element.name === 'YX1 WIRELESS EARPHONES'){
-            return(
-                <p>{element.description}</p>
-            )
-        }else{
-            return(
-                <p>Hello</p>
-            )
-        }
-    })
-
-
 }
 export default Product_Detail
